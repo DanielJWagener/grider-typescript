37191f4a5b4d425705cf9d3fc80f7ee9bf3ee874
DanielJWagener <daniel.j.wagener@gmail.com>
2023-10-25 16:54:23 -0400

    Finish maps project

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,5 +1,14 @@
 /// <reference types="@types/google.maps" />
 
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+  color: string;
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -15,5 +24,23 @@ export class CustomMap {
       zoom: 1
     });
     return new CustomMap(customMap);
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
+      }
+    });
+
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent()
+      });
+
+      infoWindow.open(this.googleMap, marker);
+    });
   }
 }
--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,7 +1,12 @@
 /// <reference types="@types/google.maps" />
-
 import { CustomMap } from "./CustomMap";
+import { User } from "./User";
+import { Company } from "./Company";
+
+const user = new User();
+const company = new Company();
 
 CustomMap.initializeMap("map").then(customMap => {
-  customMap.googleMap;
+  customMap.addMarker(user);
+  customMap.addMarker(company);
 });
