1a00680d29b4ab4eef03997146f245cacddd2252
DanielJWagener <daniel.j.wagener@gmail.com>
2023-12-01 17:08:32 -0500

    Create Sortable class

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,3 +1,24 @@
-console.log("oy");
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
-let foo: string = "bar";
+const numbersCollection = new NumbersCollection([10, 4, -5, 0]);
+const sorter = new Sorter(numbersCollection);
+sorter.sort();
+console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection("WaXd");
+const charSorter = new Sorter(charactersCollection);
+charSorter.sort();
+console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const listSorter = new Sorter(linkedList);
+listSorter.sort();
+linkedList.print();
