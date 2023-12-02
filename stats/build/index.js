829ec8af4dd2f253789acaf57b7474457665b6a0
DanielJWagener <daniel.j.wagener@gmail.com>
2023-12-01 22:58:04 -0500

    Read CSV file with generics and abstract class

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log("Hi there");
+Object.defineProperty(exports, "__esModule", { value: true });
+const MatchReader_1 = require("./MatchReader");
+const MatchResult_1 = require("./MatchResult");
+const reader = new MatchReader_1.MatchReader("football.csv");
+reader.read();
+console.log(reader.data);
+let manUnitedWins = 0;
+for (let match of reader.data) {
+    let [_date, homeTeam, awayTeam, _homeScore, _awayScore, matchResult, _referee] = match;
+    if (homeTeam === "Man United" && matchResult === MatchResult_1.MatchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (awayTeam === "Man United" && matchResult === MatchResult_1.MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+}
+console.log(`Manchester United won ${manUnitedWins} matches.`);
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hi there");
+import { MatchReader } from "./MatchReader";
+import { MatchResult } from "./MatchResult";
+
+const reader = new MatchReader("football.csv");
+reader.read();
+
+console.log(reader.data);
+
+let manUnitedWins = 0;
+
+for (let match of reader.data) {
+  let [_date, homeTeam, awayTeam, _homeScore, _awayScore, matchResult, _referee] = match;
+
+  if (homeTeam === "Man United" && matchResult === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (awayTeam === "Man United" && matchResult === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Manchester United won ${manUnitedWins} matches.`);
