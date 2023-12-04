b0b6289a80da020a9ac2415917ee936680b9cafb
DanielJWagener <daniel.j.wagener@gmail.com>
2023-12-04 08:10:15 -0500

    Add sessions

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,12 @@
+import bodyParser from "body-parser";
+import cookieSession from "cookie-session";
 import express from "express";
 import { router } from "./routes/loginRoutes";
-import bodyParser from "body-parser";
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ["asdfq"] }));
 app.use(router);
 
 app.listen(3000, () => {
